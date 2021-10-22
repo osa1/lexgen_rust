919d7945268b3b34b2df323e1c74dc4c246953c2
Ömer Sinan Ağacan <omeragacan@gmail.com>
2021-10-22 14:38:44 +0300

    Make it an executable for now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let file = &args[1];
+    let contents = std::fs::read_to_string(file).unwrap();
+    let lexer = Lexer::new(&contents);
+    let tokens: Vec<Token> = lexer.map(|t| t.unwrap().1).collect();
+    println!("{:?}", tokens);
+}
+
 use lexgen::lexer;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
