f14a7a525194dcf3de487f0e1c3cbb2559dc92d2
Ömer Sinan Ağacan <omeragacan@gmail.com>
2021-11-23 09:39:24 +0300

    Skip byte order mark when parsing a file

--- crates/dump_tokens/src/main.rs
+++ crates/dump_tokens/src/main.rs
@@ -4,7 +4,14 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
     let file = &args[1];
     let contents = std::fs::read_to_string(file).unwrap();
-    let lexer = Lexer::new(&contents);
+    let mut contents = contents.as_str();
+
+    // Skip byte order mark
+    if contents.len() > 3 && contents.as_bytes()[..3] == [0xEF, 0xBB, 0xBF] {
+        contents = &contents[3..];
+    }
+
+    let lexer = Lexer::new(contents);
     let tokens: Vec<Token> = lexer.map(|t| t.unwrap().1).collect();
     println!("{:?}", tokens);
 }
