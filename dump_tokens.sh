ed246d6089e455092854097d8e1a6d4c751e8048
Ömer Sinan Ağacan <omeragacan@gmail.com>
2021-11-23 09:51:55 +0300

    dump_tokens.sh: handle whitespace in file path, run in release mode

--- dump_tokens.sh
+++ dump_tokens.sh
@@ -6,9 +6,9 @@ shopt -s globstar
 
 TARGET=$1
 
-cargo build
+cargo build --release
 
 for rust_file in $TARGET/**/*.rs; do
     echo $rust_file
-    ./target/debug/dump_tokens $rust_file > /dev/null
+    ./target/release/dump_tokens "$rust_file" > /dev/null
 done
